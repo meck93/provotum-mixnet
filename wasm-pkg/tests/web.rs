a7cb8e03d30a8f48b7bdef44ed237c5286ac77e3
Moritz Eck <moritz.eck@gmail.com>
2020-10-19 11:53:57 +0200

    ✨ (WASM: Multiplication): adds Rust multiplication function -> call from JS

--- wasm-pkg/tests/web.rs
+++ wasm-pkg/tests/web.rs
@@ -4,10 +4,21 @@
 
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
+use wasm_package::multiply;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
+fn smoke_test() {
     assert_eq!(1 + 1, 2);
+}
+
+#[wasm_bindgen_test]
+fn test_multiply() {
+    let n1: u64 = 12312312312312;
+    let n2: u64 = 12312312312312;
+
+    let result = multiply(n1, n2);
+
+    assert_eq!(result, result);
 }
