542b7fa94fe46957223d68253a3c6c543b5a3788
Moritz Eck <moritz.eck@gmail.com>
2020-10-27 12:40:57 +0100

    ⏪ (bc-node): revert: change_spec.rs to default + update: custom chain spec

--- bc-node/node/src/chain_spec.rs
+++ bc-node/node/src/chain_spec.rs
@@ -49,13 +49,15 @@ pub fn development_config() -> Result<ChainSpec, String> {
             testnet_genesis(
                 wasm_binary,
                 // Initial PoA authorities
-                vec![authority_keys_from_seed("ZH")],
+                vec![authority_keys_from_seed("Alice")],
                 // Sudo account
-                get_account_id_from_seed::<sr25519::Public>("ZH"),
+                get_account_id_from_seed::<sr25519::Public>("Alice"),
                 // Pre-funded accounts
                 vec![
-                    get_account_id_from_seed::<sr25519::Public>("ZH"),
+                    get_account_id_from_seed::<sr25519::Public>("Alice"),
                     get_account_id_from_seed::<sr25519::Public>("Bob"),
+                    get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+                    get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
                 ],
                 true,
             )
@@ -78,24 +80,34 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 
     Ok(ChainSpec::from_genesis(
         // Name
-        "Local Testnet Voting",
+        "Local Testnet",
         // ID
-        "local_testnet_voting",
+        "local_testnet",
         ChainType::Local,
         move || {
             testnet_genesis(
                 wasm_binary,
                 // Initial PoA authorities
                 vec![
-                    authority_keys_from_seed("ZH"),
-                    authority_keys_from_seed("AG"),
+                    authority_keys_from_seed("Alice"),
+                    authority_keys_from_seed("Bob"),
                 ],
                 // Sudo account
-                get_account_id_from_seed::<sr25519::Public>("ZH"),
+                get_account_id_from_seed::<sr25519::Public>("Alice"),
                 // Pre-funded accounts
                 vec![
-                    get_account_id_from_seed::<sr25519::Public>("ZH"),
-                    get_account_id_from_seed::<sr25519::Public>("AG"),
+                    get_account_id_from_seed::<sr25519::Public>("Alice"),
+                    get_account_id_from_seed::<sr25519::Public>("Bob"),
+                    get_account_id_from_seed::<sr25519::Public>("Charlie"),
+                    get_account_id_from_seed::<sr25519::Public>("Dave"),
+                    get_account_id_from_seed::<sr25519::Public>("Eve"),
+                    get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+                    get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+                    get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+                    get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+                    get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+                    get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+                    get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
                 ],
                 true,
             )
